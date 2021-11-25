5e6501c9750da76e74f4be20068fac6edb0cedc0
Paras Chaudhary <paras.chaudhary.cse19@itbhu.ac.in>
2021-11-25 18:46:24 +0530

    Revert "Revert ".""
    This reverts commit f426b6ecf5108371d07ecdd86e5108d5750a4e65.

--- src/main/java/com/alchemy/dbms/DatabaseConfig.java
+++ src/main/java/com/alchemy/dbms/DatabaseConfig.java
@@ -10,11 +10,19 @@ public class DatabaseConfig {
 
   @Value("${spring.datasource.url}")
   private String dbUrl;
+  
+  @Value("${spring.datasource.username}")
+  private String dbUser;
+  
+  @Value("${spring.datasource.password}")
+  private String dbpass;
 
   @Bean
   public DataSource dataSource() {
       HikariConfig config = new HikariConfig();
       config.setJdbcUrl(dbUrl);
+      config.setUsername(dbUser);
+      config.setPassword(dbpass);
       return new HikariDataSource(config);
   }
 }
